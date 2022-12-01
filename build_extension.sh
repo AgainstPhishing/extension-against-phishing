104fb90fafff1bdde8e43fad7245565fab51025b
Jakub A. W <jakubwasek@gmail.com>
2022-12-01 16:26:09 +0100

    Cross-platform version

--- build_extension.sh
+++ build_extension.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
-cd browserextension
 
-[ -e ../extension-against-phishing-build.xpi ] && rm ../extension-against-phishing-build.xpi
-zip -r ../extension-against-phishing-build.xpi ./*
+{
+if [ -f extension-building.lock ]; then
+    echo "Building locked! Aborted..."
+    exit 0
+fi
+}
+# Add lock
+touch extension-building.lock
+
+# Prepare
+cd src
+
+# build Chrome
+[ -e ../extension-against-phishing-build-chrome.xpi ] && rm ../extension-against-phishing-build-chrome.xpi
+zip -r ../extension-against-phishing-build-chrome.xpi ./* -x manifests/\* manifest.json
+cd manifests/chromium
+zip ../../../extension-against-phishing-build-chrome.xpi manifest.json
+cd ../../
+
+echo "Chromium build finished!";
+
+
+# build Firefox
+[ -e ../extension-against-phishing-build-firefox.xpi ] && rm ../extension-against-phishing-build-firefox.xpi
+zip -r ../extension-against-phishing-build-firefox.xpi ./* -x manifests/\* manifest.json
+cd manifests/firefox
+zip ../../../extension-against-phishing-build-firefox.xpi manifest.json
+cd ../../
+
+echo "Firefox build finished!";
+
+
+# clean
+cd ..
+
+# remove lock
+rm extension-building.lock
--- src/against-blacklist.js
+++ src/against-blacklist.js
@@ -1,22 +1,15 @@
-let psl;
-(async () => {
-  const src = chrome.runtime.getURL("lib/psl.min.js");
-  const moduleObject = await import(src);
-  psl = moduleObject.default;
-  console.log("moduleObject", moduleObject);
-})();
-
 function throwWarning() {
   window.location.href = 'https://phishing-blocked.surge.sh/?from=' + window.location.href;
 }
 
-
-
-const isItDomainOrSubdomainOfWhitelistedProject = (project) => (
-  project.domain === window.location.hostname || window.location.hostname.split("").reverse().join("").indexOf(project.domain.split("").reverse().join("")+".") === 0
-);
+const isUserVisitBlacklistedDomain = (blacklistDomains) => {
+  const domain = psl.parse(window.location.hostname).domain;
+  console.log("isUserVisitBlacklistedDomain", domain, blacklistDomains);
+  return -1 !== blacklistDomains.findIndex(blacklistDomain => blacklistDomain === domain);
+};
 
 chrome.storage.local.get('blacklistDomains', ({blacklistDomains}) => {
-  console.log("blacklistDomains", blacklistDomains);
-  console.log("psl", psl);
+  if(isUserVisitBlacklistedDomain(blacklistDomains)) {
+    throwWarning();
+  }
 });
